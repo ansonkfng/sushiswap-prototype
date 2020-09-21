090e5507151c515ff09f0f9cd0cffb49ade0fa4a
Anson Ng <ansonng@astri.org>
2020-09-21 22:20:31 +0800

    first deploy

---
+++ src/constants/tokenAddresses.ts
@@ -0,0 +1,4 @@
+export const sushi = '0x54aaC96261e2DB68aF34c1cD4b972D59fdAa0c4a'
+export const sushiv2 = '0x54aaC96261e2DB68aF34c1cD4b972D59fdAa0c4a'
+export const sushiAddress = '0x54aaC96261e2DB68aF34c1cD4b972D59fdAa0c4a'
+export const masterChefAddress = '0xB9E343775109f8e43D70979BE394515136141C21'
